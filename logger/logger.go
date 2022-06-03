15ab5af9ed46687288401b97c5aa7048dec93f4f
Rupak Work <rupak.kumar.ambasta@gmail.com>
2022-06-03 14:22:38 +0530

    go

---
+++ logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+)
+
+type ProductionLogger struct {
+	zap *zap.Logger
+}
+
+func New() *ProductionLogger {
+	l := zap.NewProduction()
+	return &ProductionLogger{l}
+}
+
+func (logger *ProductionLogger) Debug(msg string) {
+	logger.zap.Debug(msg)
+}
+
+func (logger *ProductionLogger) Warn(msg string) {
+	logger.zap.Warn(msg)
+}
+
+func (logger *ProductionLogger) Error(msg string) {
+	logger.zap.Error(msg)
+}
+
+func (logger *ProductionLogger) Info(msg string) {
+	logger.zap.Info(msg)
+}
