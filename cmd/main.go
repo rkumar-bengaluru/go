93cb3f23d56531b9f4271501baa4ad82c8abd12b
Rupak Work <rupak.kumar.ambasta@gmail.com>
2022-06-03 15:55:15 +0530

    go

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/rkumar-bengaluru/go/golog"
-	"github.com/rkumar-bengaluru/go/gologconsumer"
 	"github.com/rkumar-bengaluru/go/logger"
 )
 
 func main() {
-	l := golog.Default()
-	l.Info("logging from main...")
-	gologconsumer.UseGoLog("using golog from gologconsumer package...")
-	logger.NewRotationLogger().Debug("from rotation logging...")
+	logger.New().Info("default logger")
+	logger.NewDevelopmentLogger().Info("from development logger")
+	logger.NewRotationLogger().Info("from rotation logger")
 }
