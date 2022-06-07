15ad05fc5ef5cee7e92c4a1fa96f36fb40597348
Rupak Work <rupak.kumar.ambasta@gmail.com>
2022-06-07 16:26:26 +0530

    go

---
+++ ioc/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.uber.org/fx"
+)
+
+func main() {
+	app := fx.New(
+		// Provide all the constructors we need, which teaches Fx how we'd like to
+		// construct the *log.Logger, http.Handler, and *http.ServeMux types.
+		// Remember that constructors are called lazily, so this block doesn't do
+		// much on its own.
+		fx.Provide(
+			NewLogger,
+			NewHandler,
+			NewMux,
+		),
+		// Since constructors are called lazily, we need some invocations to
+		// kick-start our application. In this case, we'll use Register. Since it
+		// depends on an http.Handler and *http.ServeMux, calling it requires Fx
+		// to build those types using the constructors above. Since we call
+		// NewMux, we also register Lifecycle hooks to start and stop an HTTP
+		// server.
+		fx.Invoke(Register),
+
+		// This is optional. With this, you can control where Fx logs
+		// its events. In this case, we're using a NopLogger to keep
+		// our test silent. Normally, you'll want to use an
+		// fxevent.ZapLogger or an fxevent.ConsoleLogger.
+		fx.WithLogger(
+			func() fxevent.Logger {
+				return fxevent.NopLogger
+			},
+		),
+	)
+
+	// In a typical application, we could just use app.Run() here. Since we
+	// don't want this example to run forever, we'll use the more-explicit Start
+	// and Stop.
+	startCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Start(startCtx); err != nil {
+		log.Fatal(err)
+	}
+
+	// Normally, we'd block here with <-app.Done(). Instead, we'll make an HTTP
+	// request to demonstrate that our server is running.
+	if _, err := http.Get("http://localhost:8080/"); err != nil {
+		log.Fatal(err)
+	}
+
+	stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Stop(stopCtx); err != nil {
+		log.Fatal(err)
+	}
+
+}
