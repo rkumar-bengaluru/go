e5eae67037d8c9d5714f506199c6f6163cb1a60d
Rupak Work <rupak.kumar.ambasta@gmail.com>
2022-06-07 18:58:22 +0530

    go

--- ioc/server/config/configreader/constants.go
+++
@@ -1,9 +0,0 @@
-package configsreader
-
-const (
-	ConfigType           = "yml"
-	ConfigName           = "config"
-	ConfigProfile        = "PROFILE"
-	ConfigProfileDefault = "local"
-	ConfigPath           = "./resources/"
-)
