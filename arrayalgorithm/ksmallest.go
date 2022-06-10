257433f4b8a34f29b35c86a9f7ed3903a68beebd
Rupak Work <rupak.kumar.ambasta@gmail.com>
2022-06-10 18:02:03 +0530

    go

---
+++ arrayalgorithm/ksmallest.go
@@ -0,0 +1,36 @@
+package arrayalgorithm
+
+import (
+	"math"
+)
+
+func removeElement(n []int, r int) []int {
+	res := []int{}
+	for i := 0; i < len(n); i++ {
+		if n[i] != r {
+			res = append(res, n[i])
+		}
+	}
+	return res
+}
+
+func FindKSmallest(n []int, k int) int {
+	smallest := math.MaxInt32
+	l := n
+	for k > 0 {
+		for i := 0; i < len(l); i++ {
+			if l[i] < smallest {
+				smallest = l[i]
+			}
+
+		}
+		l = removeElement(l, smallest)
+
+		if k != 1 {
+			smallest = math.MaxInt32
+		}
+		k--
+	}
+
+	return smallest
+}
